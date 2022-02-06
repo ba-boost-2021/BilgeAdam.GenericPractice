f863e5907db02b786a5e619d1a9d3485f42bb66b
ba-boost-2021 <99113350+ba-boost-2021@users.noreply.github.com>
2022-02-06 17:13:58 +0300

    Add files via upload

---
+++ BilgeAdam.GenericPractice2/Helpers/TypeLogger.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace BilgeAdam.GenericPractice2.Helpers
+{
+    public class TypeLogger
+    {
+        public string GetTypeProperties<T>()
+        {
+            var type = typeof(T);
+            var str = new StringBuilder();
+            str.Append(type.FullName + ",");
+            str.Append(type.Name + ",");
+            if (type.IsClass) str.Append("Class,");
+            if (type.IsEnum) str.Append("Enum,");
+            if (type.IsInterface) str.Append("Interface,");
+            if (type.IsAbstract) str.Append("Abstract,");
+            if (type.IsPrimitive) str.Append("Primitive,");
+            if (type.IsValueType) str.Append("ValueType,");
+            var interfaces = type.GetInterfaces();
+            if (interfaces.Any())
+            {
+                foreach (var _interface in interfaces)
+                {
+                    str.Append(_interface.Name + ",");
+                }
+            }
+            //var type = typeof(T);
+            //var strList = new List<string>();
+            //strList.Add(type.FullName);
+            //strList.Add(type.Name);
+            //strList.Add(type.FullName);
+            //strList.Add(type.FullName);
+            //strList.Add(type.FullName);
+            //if (type.IsClass) strList.Add("Class");
+            //var interfaces = type.GetInterfaces();
+            //if (interfaces.Any())
+            //{
+            //    foreach (var _interface in interfaces)
+            //    {
+            //        strList.Add(_interface.Name);
+            //    }
+            //}
+
+            //return string.Join(',', strList);
+            return str.ToString();
+        }
+    }
+}
---
+++ BilgeAdam.GenericPractice2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BilgeAdam.GenericPractice2
+{
+    class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+}
